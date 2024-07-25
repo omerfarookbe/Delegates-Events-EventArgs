d06602a4e7271530560c317c1cc6481346cad5a1
Omer Farook <49291618+omerfarookbe@users.noreply.github.com>
2024-07-25 15:39:15 -0400

    Add files via upload

---
+++ Delegates-Events-EventArgs/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program()
+{
+    public static void Main(string[] args)
+    {
+        Action<string> messageTarget;
+        messageTarget = ShowMessage;
+        messageTarget("Hello");
+
+        var worker = new Worker();
+        worker.WorkPerformed += (s, e) =>
+        {
+            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType.ToString());
+        };
+
+        worker.WorkPerformed += (s, e) => Console.WriteLine("Work completed");
+
+        worker.DoWork(8, WorkType.coding);
+
+        Console.Read();
+    }
+
+    private static void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+}
+
+public enum WorkType
+{
+    meeting,
+    coding,
+    supporting,
+}
---
+++ Delegates-Events-EventArgs/WorkPerformedEventArgs.cs
@@ -0,0 +1,8 @@
+﻿public class WorkPerformedEventArgs
+{
+    public int Hours { get; set; }
+
+    public WorkType WorkType { get; set; }
+
+    public WorkPerformedEventArgs(int _hours, WorkType _worktype) => (Hours, WorkType) = (_hours, _worktype);
+}
